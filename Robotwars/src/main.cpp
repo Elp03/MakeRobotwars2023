58f5d1d1743fb34fdc5d8731ee14f03145a34b32
Elp03 <helle@grasmo.no>
2023-09-23 14:03:17 +0200

    Made it pretty

--- Robotwars/src/main.cpp
+++ Robotwars/src/main.cpp
@@ -7,23 +7,35 @@
 #define pin_wepon_state 36
 #define pin_wepon_speed 33
 
-bool robot_state;
-int robot_y_axis;
-int robot_x_axis;
-bool wepon_state;
-int wepon_speed;
+
 
 
 struct struct_message
 {
   int message_robot_x_axis;
   int message_robot_y_axis; 
-  bool message_robot_on_state;
-  bool message_wepon_on_state;
-  int message_wepon_speed_state;
+  bool message_robot_speed;
+  bool message_wepon_state;
+  int message_wepon_speed;
+};
+
+struct robot_values 
+{
+  bool state;
+  int y_axis;
+  int x_axis;
+};
+
+struct wepon_values 
+{
+  bool state;
+  int speed;
 };
 
 struct_message conntroller_message;
+
+robot_values robot;
+wepon_values wepon;
 
 void setup() {
   // put your setup code here, to run once:
@@ -38,16 +50,32 @@ void setup() {
 }
 
 void loop() {
-  robot_state =  digitalRead(pin_robot_state);
-  robot_x_axis = analogRead(pin_robot_x_axis);
-  robot_y_axis = analogRead(pin_robot_y_axis);
-  wepon_state = digitalRead(pin_wepon_state);
-  wepon_speed = analogRead(pin_wepon_speed);
+  update_data(); 
+  update_struct();
 
-  Serial.print("Robot state:"); Serial.println(robot_state);
-  Serial.print("Robot x:"); Serial.println(robot_x_axis);
-  Serial.print("Robot y:"); Serial.println(robot_y_axis);
-  Serial.print("wepon state:"); Serial.println(wepon_state);
-  Serial.print("wepon pot:"); Serial.println(wepon_speed);
   delay(1000);
+}
+
+void update_data(){
+  robot.state =  digitalRead(pin_robot_state);
+  robot.x_axis = analogRead(pin_robot_x_axis);
+  robot.y_axis = analogRead(pin_robot_y_axis);
+  wepon.state= digitalRead(pin_wepon_state);
+  wepon.speed = analogRead(pin_wepon_speed);
+}
+
+void update_struct(){
+  conntroller_message.message_robot_speed = (robot.state ? true : false);
+  conntroller_message.message_robot_x_axis = robot.x_axis;
+  conntroller_message.message_robot_y_axis = robot.y_axis;
+  conntroller_message.message_wepon_state = (wepon.state ? true : false);
+  conntroller_message.message_wepon_speed = wepon.speed;
+}
+
+void print_values(){
+  Serial.print("Robot state:"); Serial.println(robot.state);
+  Serial.print("Robot x:"); Serial.println(robot.x_axis);
+  Serial.print("Robot y:"); Serial.println(robot.y_axis);
+  Serial.print("wepon state:"); Serial.println(wepon.state);
+  Serial.print("wepon pot:"); Serial.println(wepon.speed);
 }
