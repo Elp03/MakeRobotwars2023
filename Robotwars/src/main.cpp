9450be7dbf825c47cdee3dbe8bb40415c5cc0493
OriginalMetaTrademarkPending <metaadmir@gmail.com>
2023-10-04 13:05:26 +0200

    Merged robot into main, fixed the code as well

--- Robotwars/src/main.cpp
+++ Robotwars/src/main.cpp
@@ -1,10 +1,16 @@
 #include <Arduino.h>
-<<<<<<< HEAD
 //sending data
 #include <esp_now.h>
 #include <WiFi.h>
-
 #include <Wire.h>
+#include "Motor.hpp"
+#include "Robot.hpp"
+#define EN1 12
+#define IN1_1 13
+#define IN1_2 14
+#define EN2 33
+#define IN2_1 26
+#define IN2_2 32
 
 // put function declarations here:
 #define pin_robot_state 39
@@ -15,6 +21,13 @@
 
 uint8_t broadcastAddress[] = {0x7C, 0x9E, 0xBD, 0x66, 0x5D, 0x38};
 
+
+const size_t buffer_size = 3;
+uint8_t EN_array[buffer_size] = {EN1, EN2, 0};
+uint8_t IN1_array[buffer_size] = {IN1_1, IN2_1, 0};
+uint8_t IN2_array[buffer_size] = {IN1_2, IN2_2, 0};
+
+Robot sbinnala(EN_array, IN1_array, IN2_array);
 
 struct struct_message
 {
@@ -92,6 +105,7 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200); 
 
+  sbinnala.init();
   pinMode(pin_robot_state, INPUT);
   pinMode(pin_robot_x_axis, INPUT);
   pinMode(pin_robot_y_axis, INPUT);
@@ -138,30 +152,6 @@ void loop() {
   else {
     Serial.println("Error sending the data");
   }
-
-=======
-#include "Motor.hpp"
-#include "Robot.hpp"
-#define EN1 12
-#define IN1_1 13
-#define IN1_2 14
-#define EN2 33
-#define IN2_1 26
-#define IN2_2 32
-
-const size_t buffer_size = 3;
-uint8_t EN_array[buffer_size] = {EN1, EN2, 0};
-uint8_t IN1_array[buffer_size] = {IN1_1, IN2_1, 0};
-uint8_t IN2_array[buffer_size] = {IN1_2, IN2_2, 0};
-
-Robot sbinnala(EN_array, IN1_array, IN2_array);
-
-void setup() {
-  sbinnala.init();
-  Serial.begin(115200);
-}
-
-void loop() {
   Serial.println("Sbin sequence started!");
   sbinnala.moveForward();
   delay(1000);
@@ -178,6 +168,5 @@ void loop() {
   sbinnala.turnRight();
   delay(1000);
   sbinnala.stop();
->>>>>>> robot
   delay(1000);
 }
