21fbd1d078918bd42f111db9572b359b3ce9b192
OriginalMetaTrademarkPending <metaadmir@gmail.com>
2023-10-15 11:46:40 +0200

    Rough draft on receiver code

--- Robotwars/src/Receiver.cpp
+++ Robotwars/src/Receiver.cpp
@@ -1,6 +1,34 @@
 #include "Receiver.hpp"
 
-Receiver::Receiver(struct_message* obj_message): message{obj_message} {}
+Receiver::Receiver(uint8_t* obj_cont_addr, struct_message* obj_message): controller_addr{obj_cont_addr}, message{obj_message} {}
+
+void Receiver::init()
+{
+    // Init ESP-NOW
+    if (esp_now_init() != ESP_OK)
+    {
+        Serial.println("Error initializing ESP-NOW");
+        return;
+    }
+
+    // Once ESPNow is successfully Init, we will register for Send CB to
+    // get the status of Trasnmitted packet
+    esp_now_register_send_cb(data_sent_cb);
+
+    // Register peer
+    memcpy(peerInfo.peer_addr, controller_addr, 6);
+    peerInfo.channel = 0;
+    peerInfo.encrypt = false;
+
+    // Add peer
+    if (esp_now_add_peer(&peerInfo) != ESP_OK)
+    {
+        Serial.println("Failed to add peer");
+        return;
+    }
+    // Register for a callback function that will be called when data is received
+    esp_now_register_recv_cb(data_recv_cb);
+}
 
 void Receiver::print_values() const
 {
@@ -13,4 +41,22 @@ void Receiver::print_values() const
 struct_message* Receiver::get_message() const
 {
     return message;
+}
+
+void Receiver::set_message()
+{
+    memcpy(this->message, gl_message, sizeof(this->message));
+}
+
+void data_sent_cb(const uint8_t* mac_addr, esp_now_send_status_t status)
+{
+    Serial.print("\r\nLast Packet Send Status:\t");
+    Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
+
+void data_recv_cb(const uint8_t *mac, const uint8_t *incomingData, int len)
+{
+    memcpy(gl_message, incomingData, sizeof(gl_message));
+    Serial.print("Bytes received: ");
+    Serial.println(len);
 }
--- Robotwars/src/Receiver.hpp
+++ Robotwars/src/Receiver.hpp
@@ -5,6 +5,9 @@
 #include "Wire.h"
 #include "WiFi.h"
 
+// Global variable to store the message from the callback
+uint8_t* gl_message;
+
 //Structure example to send data
 //Must match the receiver structure
 struct struct_message
@@ -20,10 +23,16 @@ class Receiver : public espnow
     private:
         uint8_t* controller_addr;
         struct_message* message;
+        esp_now_peer_info_t peerInfo;
     public:
-        Receiver(struct_message* obj_message);
+        Receiver(uint8_t* obj_cont_addr, struct_message* obj_message);
+        void init();
         void print_values() const;
         struct_message* get_message() const;
+        void set_message();
 };
+
+void data_sent_cb(const uint8_t *mac_addr, esp_now_send_status_t status);
+void data_recv_cb(const uint8_t *mac, const uint8_t *incomingData, int len);
 
 #endif /*RECEIVER_H*/
